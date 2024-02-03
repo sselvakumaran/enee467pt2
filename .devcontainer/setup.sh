58358062c5b66409e4220c45e21814907554336e
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-02-02 22:19:42 -0500

    Disabled cloning Gazebo simulation package since a custom one's made

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,7 +10,7 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
   ros-humble-ros2-control \
   ros-humble-ros2-controllers
 
-git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Gazebo_Simulation.git src/Universal_Robots_ROS2_Gazebo_Simulation
+# git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Gazebo_Simulation.git src/Universal_Robots_ROS2_Gazebo_Simulation
 
 # Install package dependencies
 rosdep update && rosdep install --from-paths src -y --ignore-src
