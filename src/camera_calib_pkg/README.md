b1acb6a9c49d5c8af3a34c66d905684a38c17caa
lorocks <lowell.lobo@hotmail.com>
2024-06-08 17:50:51 -0400

    Fix launch file to access usb camera and perform aruco detection properly

---
+++ src/camera_calib_pkg/README.md
@@ -0,0 +1,6 @@
+# Required Extra Package Installations
+```bash
+  sudo apt-get install ros-humble-usb-cam
+  sudo apt-get install ros-humble-aruco-opencv
+
+```
--- src/camera_calib_pkg/launch/extrinsic_calibration.launch.py
+++ src/camera_calib_pkg/launch/extrinsic_calibration.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, RegisterEventHandler, ExecuteProcess, DeclareLaunchArgument
-from launch.event_handlers import OnProcessExit
+from launch.event_handlers import OnProcessExit, OnExecutionComplete, OnProcessStart
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, FindExecutable
@@ -53,20 +53,22 @@ def generate_launch_description():
         package_dir, 'calibration/camera', f'{camera_name_var}.yaml'
     )
 
+    width = LaunchConfiguration('width', default='1280')
+    height = LaunchConfiguration('height', default='720')
+
     start_usb_camera = Node(
         package='usb_cam',
         executable='usb_cam_node_exe',
-        name=camera_device_var,
+        name=camera_name_var,
         parameters=[
                 {
                     'video_device': camera_device_var,
-                    'image_width': '1920',
-                    'image_height': '1080',
-                    'pixel_format': 'yuyv',
-                    'camera_frame_id': 'tracking_cam',
+                    'image_width': width,
+                    'image_height': height,
+                    'pixel_format': 'mjpeg2rgb',
+                    'frame_id': 'tracking_cam',
                     'camera_name': camera_name_var,
-                    'io_map': 'mmap',
-                    'camera_info_url': yaml_params,
+                    # 'camera_info_url': yaml_params,
                 }
             ],
         output='screen',
@@ -74,14 +76,14 @@ def generate_launch_description():
 
     desc.add_action(start_usb_camera)
 
-    start_rectification = Node(
-        package='image_proc',
-        executable='image_proc_exe',
-        name='rectify_image',
-        namespace=camera_name_var,
-    )
+    # start_rectification = Node(
+    #     package='image_proc',
+    #     executable='image_proc_exe',
+    #     name='rectify_image',
+    #     namespace=camera_name_var,
+    # )
 
-    # desc.add_action(start_rectification)
+    # # desc.add_action(start_rectification)
 
     # Need to add aruco detection
     start_aruco_detect = Node(
@@ -90,7 +92,7 @@ def generate_launch_description():
         parameters=[
             {
                 'cam_base_topic': 'image_raw',
-                'marker_size': marker_size
+                'marker_size': marker_size_var
             }
         ],
         output='screen'
@@ -109,9 +111,9 @@ def generate_launch_description():
     )
 
     delay_configure = RegisterEventHandler(
-        event_handler=OnProcessExit(
+        event_handler=OnProcessStart(
             target_action=start_aruco_detect,
-            on_exit=[configure_lifecycle],
+            on_start=[configure_lifecycle],
         )
     )
 
