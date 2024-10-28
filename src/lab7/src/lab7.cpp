927527362aab7a58ddb2447534ae44839214433a
Abhishekh Reddy <126507842+armgits@users.noreply.github.com>
2024-10-28 17:48:56 -0400

    Updated lab 7 source file

--- src/lab7/src/lab7.cpp
+++ src/lab7/src/lab7.cpp
@@ -7,21 +7,24 @@ void findMeanErrorVector(
   if (error_vecs_in.empty())
     return;
 
+  mean_error_vec_out = Eigen::Vector<double, 7>::Zero();
+
   /// TODO: Find the mean error vector by averaging the error vectors.
 
-  mean_error_vec_out = Eigen::Vector<double, 7>::Zero(); // Update this with calculated value
 }
 
 void findCovarianceMatrix(
   const std::vector<Eigen::Vector<double, 7>>& error_vecs_in,
+  const Eigen::Vector<double, 7>& mean_error_vec_in,
   Eigen::Matrix<double, 7, 7>& covariance_matrix_out)
 {
   if (error_vecs_in.empty())
     return;
 
+  covariance_matrix_out = Eigen::Matrix<double, 7, 7>::Zero(); // Update this with calculated value
+
   /// TODO: Find the covariance matrix using the error vectors.
 
-  covariance_matrix_out = Eigen::Matrix<double, 7, 7>::Zero(); // Update this with calculated value
 }
 
 void findLeastSquaresError(
@@ -31,18 +34,19 @@ void findLeastSquaresError(
   if (error_vecs_in.empty())
     return;
 
+  least_squares_vec_out = Eigen::Vector<double, 7>::Zero();
+
   /**
-   * TODO: Apply the least squares method to each component of the error vector separately, and
-   *       then combine the results to form the least squares error vector.
+   * TODO: Apply the least squares method to each component of the error vector separately to form
+   *       the least squares error vector.
    */
 
-  least_squares_vec_out = Eigen::Vector<double, 7>::Zero(); // Update this with calculated value
 }
 
 void HandEyeCalibNode::verifyCalibration()
 {
   /**
-   * Measurement variables available for use:
+   * Measurement variables:
    *
    * - Reference positions of the end-effector are stored in `actual_eef_positions_`
    * - Reference orientations of the end-effector are stored in `actual_eef_orientations_`
@@ -52,16 +56,28 @@ void HandEyeCalibNode::verifyCalibration()
    */
 
   std::vector<Eigen::Vector<double, 7>> error_vecs {};
+
   /**
-   * TODO: Populate this variable with error vectors from the measurements which consists of:
-   *       - 3 components of position error vector
-   *       - 4 components of orientation error quaternion
-   *           (Check if the magnitude of this quaternion is close to 1)
+   * Error vectors from the measurements consists of:
+   *   - 3 components of position error vector
+   *   - 4 components of orientation error quaternion
+   *     (Check if the magnitude of this quaternion is close to 1)
    */
+  for (unsigned int i {0}; i < estimated_eef_orientations_.size(); i++) {
+    Eigen::Vector<double, 7> estimated_pose_vec {};
+    estimated_pose_vec << estimated_eef_positions_.at(i), estimated_eef_orientations_.at(i).coeffs();
+
+    Eigen::Vector<double, 7> actual_pose_vec {};
+    actual_pose_vec << actual_eef_positions_.at(i), actual_eef_orientations_.at(i).coeffs();
+
+    error_vecs.push_back(estimated_pose_vec - actual_pose_vec);
+  }
 
   findMeanErrorVector(error_vecs, mean_error_vector_);
-  findCovarianceMatrix(error_vecs, covariance_matrix_);
+  findCovarianceMatrix(error_vecs, mean_error_vector_, covariance_matrix_);
   findLeastSquaresError(error_vecs, least_squares_vector_);
+
+  RCLCPP_INFO(this->get_logger(), "Verification results can be saved now.");
 
   is_verification_complete_ = true;
 }
