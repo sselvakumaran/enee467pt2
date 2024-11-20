d75a7b5e733b793699beb8822252cb90e80e84f7
sselvakumaran <srimans@umd.edu>
2024-11-20 19:56:55 +0000

    next lab updates

--- src/lab8/src/lab8.cpp
+++ src/lab8/src/lab8.cpp
@@ -6,7 +6,8 @@ void UR3eMoveInterface::drawCircleXY(double radius_meters)
     radius_meters = 0.45;
 
   /// TODO: Move the arm into a pose to draw the shape by setting a joint-space target
-  std::vector<double> target_joint_positions {M_PI_2, -M_PI_4, M_PI_2, -3*M_PI_4, -M_PI_2, 0};
+
+  std::vector<double> target_joint_positions {0, 0, 0, -M_PI_2, -M_PI_2, 0};
 
   move_group_interface_->setJointValueTarget(target_joint_positions);
 
@@ -15,34 +16,7 @@ void UR3eMoveInterface::drawCircleXY(double radius_meters)
 
   if (joint_space_plan_success)
     move_group_interface_->execute(motion_plan_joints);
-
-  /// TODO: Set the points on the circle as waypoints and execute a Cartesian plan
-  std::vector<geometry_msgs::msg::Pose> waypoints;
-
-  auto current_pose = move_group_interface_->getCurrentPose().pose;
-  waypoints.push_back(current_pose);
-
-  int RESOLUTION = 360;
-  double DELTA_THETA = 2 * M_PI / RESOLUTION;
-  auto center = current_pose;
-  center.position.y -= radius_meters;
-
-  // auto new_pose = center;
-  // new_pose.position.y -= 0.2;
-  // waypoints.push_back(new_pose);
-
-  for (int i = 0; i <= RESOLUTION + 10; i++) {
-    auto new_pose = center;
-    auto theta = M_PI_2 + DELTA_THETA * (double) i;
-    new_pose.position.x += radius_meters*cos(theta);
-    new_pose.position.y += radius_meters*sin(theta);
-    waypoints.push_back(new_pose);
-  }
-
-  RCLCPP_WARN(this->get_logger(), ".");
-
-  moveit_msgs::msg::RobotTrajectory cartesian_trajectory;
-  bool cartesian_plan_success {planCartesianPath(waypoints, cartesian_trajectory)};
+  /// TODO: Set the corners of the square as waypoints and execute a Cartesian plan
 
   auto track_request {std::make_shared<lab8::srv::TrackRequest::Request>()};
 
@@ -57,9 +31,18 @@ void UR3eMoveInterface::drawCircleXY(double radius_meters)
 
   bool track_request_success {sendEEFTrackRequest(track_request)};
 
-  // Step 6
-  if (cartesian_plan_success && track_request_success)
-    move_group_interface_->execute(cartesian_trajectory);
+
+  for (int i = 0; i <= 8; i++) {
+    std::vector<double> target_joint_positions {i*M_PI_4, 0, 0, -M_PI_2, -M_PI_2, 0};
+
+    move_group_interface_->setJointValueTarget(target_joint_positions);
+
+    moveit::planning_interface::MoveGroupInterface::Plan motion_plan_joints;
+    bool joint_space_plan_success {planToJointSpaceGoal(target_joint_positions, motion_plan_joints)};
+
+    if (joint_space_plan_success)
+      move_group_interface_->execute(motion_plan_joints);
+  }
 
   track_request->status = lab8::srv::TrackRequest::Request::STOP;
   sendEEFTrackRequest(track_request);
@@ -99,7 +82,7 @@ void UR3eMoveInterface::drawCircleYZ(double radius_meters)
   auto center = current_pose;
   center.position.z -= radius_meters;
 
-  for (int i = 0; i <= RESOLUTION + 10; i++) {
+  for (int i = 0; i <= RESOLUTION + 2; i++) {
     auto new_pose = center;
     auto theta = M_PI_2 + DELTA_THETA * (double) i;
     new_pose.position.y += radius_meters*cos(theta);
@@ -141,9 +124,8 @@ void UR3eMoveInterface::drawSquareXY(double side_meters)
 {
   if (side_meters <= 0)
     side_meters = (0.45 * 2) / sqrt(2);
-
   /// TODO: Move the arm into a pose to draw the shape by setting a joint-space target
-  std::vector<double> target_joint_positions {M_PI_2, -M_PI_4, M_PI_2, -3*M_PI_4, -M_PI_2, 0};
+  std::vector<double> target_joint_positions {-1.0*M_PI_4, -(7.0/8.0)*M_PI, -(2.0/8.0)*M_PI, -M_PI_2 + (1.0/8.0)*M_PI, -M_PI_2, 0};
 
   move_group_interface_->setJointValueTarget(target_joint_positions);
 
@@ -159,19 +141,19 @@ void UR3eMoveInterface::drawSquareXY(double side_meters)
   waypoints.push_back(current_pose);
 
   auto pose1 = current_pose;
-  pose1.position.x += side_meters;
+  pose1.position.y -= side_meters;
   waypoints.push_back(pose1);
 
   auto pose2 = pose1;
-  pose2.position.y -= side_meters;
+  pose2.position.x -= side_meters;
   waypoints.push_back(pose2);
 
   auto pose3 = pose2;
-  pose3.position.x -= side_meters;
+  pose3.position.y += side_meters;
   waypoints.push_back(pose3);
 
   auto pose4 = pose3;
-  pose4.position.y += side_meters;
+  pose4.position.x += side_meters;
   waypoints.push_back(pose4);
 
   // Step 4
